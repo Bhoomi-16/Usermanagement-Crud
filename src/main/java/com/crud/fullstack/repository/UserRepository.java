1e2073a8c8b515822760c47f15a6143ebcfa6489
Shree Annapoorna <Bhoomi-16>
2025-06-24 17:17:00 +0530

    initial commit of Springboot and React Project

---
+++ src/main/java/com/crud/fullstack/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.crud.fullstack.repository;
+
+import com.crud.fullstack.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
